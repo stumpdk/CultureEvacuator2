b351f07a17ccba43b55cc841592f5e165c9cc305
Bo Henriksen <stumpdenmark@hotmail.com>
2014-09-28 00:05:02 +0200

    New database structure

--- Bootstrapper.php
+++ Bootstrapper.php
@@ -8,7 +8,7 @@
 	include_once('./JSONIterator.php');
 	include_once('./NamedEntityRecognizer.php');
     
-    error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    error_reporting(E_ALL);
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
     
--- database_structure.sql
+++ database_structure.sql
@@ -1,126 +1,180 @@
-CREATE DATABASE  IF NOT EXISTS `kbharkiv` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `kbharkiv`;
--- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.2.2
+-- http://www.phpmyadmin.net
 --
--- Host: Kbhkilder.dk    Database: kbharkiv
--- ------------------------------------------------------
--- Server version	5.6.17-65.0-587.wheezy
+-- Host: localhost
+-- Generation Time: Sep 28, 2014 at 12:04 AM
+-- Server version: 5.6.17-65.0-587.wheezy
+-- PHP Version: 5.4.4-14+deb7u10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `kbharkiv`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ce_comments`
 --
 
-DROP TABLE IF EXISTS `ce_comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ce_comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `ce_comments` (
+`id` int(11) NOT NULL,
   `post_id` varchar(45) DEFAULT NULL,
   `fb_id` int(11) DEFAULT NULL,
   `message` varchar(45) DEFAULT NULL,
   `created_time` datetime DEFAULT NULL,
   `like_count` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `user_name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds comments to posts';
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `user_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds comments to posts' AUTO_INCREMENT=9627 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ce_keywords`
 --
 
-DROP TABLE IF EXISTS `ce_keywords`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ce_keywords` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `ce_keywords` (
+`id` int(11) NOT NULL,
   `keyword` varchar(100) DEFAULT NULL,
-  `total_count` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds keywords for comments.';
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `total_count` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds keywords for comments.' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ce_keywords_comments`
 --
 
-DROP TABLE IF EXISTS `ce_keywords_comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ce_keywords_comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `ce_keywords_comments` (
+`id` int(11) NOT NULL,
   `keyword_id` int(11) DEFAULT NULL,
-  `comment_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds informations about keywords in comments.';
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `comment_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds informations about keywords in comments.' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ce_likes`
 --
 
-DROP TABLE IF EXISTS `ce_likes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ce_likes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `ce_likes` (
+`id` int(11) NOT NULL,
   `user_name` varchar(100) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds data about likes.';
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `fb_id` int(11) DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds data about likes.' AUTO_INCREMENT=20069 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ce_posts`
 --
 
-DROP TABLE IF EXISTS `ce_posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ce_posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ce_picture` varchar(100) DEFAULT NULL,
-  `ce_link` varchar(100) DEFAULT NULL,
-  `ce_created_time` datetime DEFAULT NULL,
-  `ce_message` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds posts from Facebook';
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE IF NOT EXISTS `ce_posts` (
+`id` int(11) NOT NULL,
+  `picture` varchar(100) DEFAULT NULL,
+  `link` varchar(100) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `message` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds posts from Facebook' AUTO_INCREMENT=858 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ce_users`
 --
 
-DROP TABLE IF EXISTS `ce_users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ce_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `ce_users` (
+`id` int(11) NOT NULL,
   `fb_id` int(11) NOT NULL,
-  `level` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds informations about users';
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  `level` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds informations about users' AUTO_INCREMENT=1 ;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ce_comments`
+--
+ALTER TABLE `ce_comments`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ce_keywords`
+--
+ALTER TABLE `ce_keywords`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ce_keywords_comments`
+--
+ALTER TABLE `ce_keywords_comments`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ce_likes`
+--
+ALTER TABLE `ce_likes`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ce_posts`
+--
+ALTER TABLE `ce_posts`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ce_users`
+--
+ALTER TABLE `ce_users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ce_comments`
+--
+ALTER TABLE `ce_comments`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9627;
+--
+-- AUTO_INCREMENT for table `ce_keywords`
+--
+ALTER TABLE `ce_keywords`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ce_keywords_comments`
+--
+ALTER TABLE `ce_keywords_comments`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ce_likes`
+--
+ALTER TABLE `ce_likes`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20069;
+--
+-- AUTO_INCREMENT for table `ce_posts`
+--
+ALTER TABLE `ce_posts`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=858;
+--
+-- AUTO_INCREMENT for table `ce_users`
+--
+ALTER TABLE `ce_users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-09-27 22:21:07
