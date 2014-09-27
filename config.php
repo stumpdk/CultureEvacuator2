8a137536f39bcf6ab2117a62b69b41c341a3e4aa
Bo Henriksen <stumpdenmark@hotmail.com>
2014-09-27 17:17:39 +0200

    Added config file

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?php
-
+	class Config{
+		public static $dbHost = 'localhost';
+		public static $dbUser = 'user';
+		public static $dbPassword = 'pass';
+		public static $dbName = 'dbName';
+		public static $dbCharset = 'utf8';
+	}
 ?>
