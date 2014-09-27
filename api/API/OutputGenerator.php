566c1b141c14138062ef40533eb18fb6c2a22da9
Bo Benjamin Blichfeldt Henriksen <stumpdenmark@hotmail.com>
2014-09-27 20:45:49 +0200

    Added API module

---
+++ api/API/OutputGenerator.php
@@ -0,0 +1,84 @@
+<?php
+
+    class OutputGenerator{
+        private $result;
+        public $results;
+        private $returnType;
+        private $output;
+        
+        public function __construct($result, $returntype)
+        {
+            $this->result = $result;
+            $this->returnType = $returntype;
+            
+            $this->createData();
+        }
+        
+        private function createData()
+        {
+            switch($this->returnType){
+                case 'JSON':
+                case 'json':
+                    $this->createJSON();
+                    break;
+                case 'GeoJSON':
+                case 'geojson':
+                    $this->createGeoJSON();
+                    break;
+            }
+            
+            $this->outputData();
+        }
+        
+        private function createJSON(){
+
+            $results = array();
+            $i = 0;
+            while($curResult = $this->result->fetch_assoc()){
+                $results[] = $curResult;
+                $i++;
+            }
+            
+            $this->results = $i;
+            $this->output = $results;
+        }
+        
+        private function startJSON()
+        {
+            $this->output .= '[';
+        }
+        
+        private function endJSON()
+        {
+            $this->output .= ']';
+        }        
+        
+        private function createGeoJSON()
+        {
+            $this->startGeoJSON();
+            
+            $this->endGeoJSON();
+        }        
+        
+        private function startGeoJSON()
+        {
+            
+        }
+        
+        private function endGeoJSON()
+        {
+        
+        }
+        
+        private function outputData(){
+            if (isset($_REQUEST["callback"])) {
+                header('Content-type: text/javascript');
+                print str_replace('{', '', $_REQUEST["callback"]) .'('.json_encode($this->output).')';
+            } else {
+                header('Content-type: application/json');
+                print json_encode($this->output);
+            }        
+        }        
+    }
+
+?>
