32c7ff1c9e08554a0efdfaff48408b43c207a4c5
Bo Henriksen <stumpdenmark@hotmail.com>
2014-09-27 17:05:07 +0200

    Added bootstrapper and static JSON iterator

---
+++ JSONIterator.php
@@ -0,0 +1,12 @@
+<?php
+	public static class JSONIterator{
+		/**
+		* Returns a JSON iterator based on an array of JSON data
+		*/
+		public static function getIterator($jsonArray){
+			$jsonIterator = new RecursiveIteratorIterator(
+		    new RecursiveArrayIterator(json_decode($jsonArray, TRUE)),
+		    RecursiveIteratorIterator::SELF_FIRST);
+		}
+	}
+?>
