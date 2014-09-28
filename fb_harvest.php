8e69eec9229cafbf90c453173f5d2ec58bba8a62
Jacob Larsen <jac@kb.dk>
2014-09-28 11:35:43 +0200

    v1.0 beta : write a json file, and can be configured to number of posts

--- fb_harvest.php
+++ fb_harvest.php
@@ -1,4 +1,13 @@
 <?php
+
+/**
+* fb_harvest.php?nor=10&group=1
+* nor = NumberOfRecords to harvest from facebook. Default is 10
+* group: 
+*   1 = Gamle København
+*   2 = Vesterbro Billeder 
+*/
+
 header('Content-Type: application/json');
 include_once('./autoload.php');
 
@@ -6,16 +15,29 @@ include_once('./autoload.php');
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
 
+// Gamle København - default group
+$groupid = "109602915873899";	
+$groupName = "Gamle København";
+$group = $_GET["group"];
+if($group == "2" ){
+	// Set to another group id
+	$groupid = "109602915873899";	// TODO: change
+	$groupName = "Vesterbro Billeder";
+}
 
-// Gamle København 
-// Skal parametiseres vha
-$groupid = "109602915873899";
+
+$outputFileName = "harvest_output.json";
+// nor = number of records
+$numberOfRecords = $_GET["nor"];
+if($numberOfRecords == ""){
+	$numberOfRecords = 10;
+}
 
 // En app som Jacob Andresen har lavet og hentet id / secret til 
 FacebookSession::setDefaultApplication('294028730792515', 'ea0876ce78b6f88d33d3cc6976882989');
 
 	// Lav den initielle høstning
-	$obj = harvestFirst(10, $groupid);
+	$obj = harvestFirst($numberOfRecords, $groupid);
 	
 	// Lab array for at kunne måle størrelsen
 	$dataArr = $obj->getProperty("data")->asArray();
@@ -23,8 +45,11 @@ FacebookSession::setDefaultApplication('294028730792515', 'ea0876ce78b6f88d33d3c
 	// "data er ...... data :-)"
 	$data = $obj->getProperty("data");
 
-	// Skriv til skærm 
-	echo print_r($data);
+	
+	 $myfile = fopen($outputFileName, "w") or die("Unable to open file!");
+	 fwrite($myfile, json_encode($dataArr));
+	 fclose($myfile);
+	 echo $numberOfRecords." poster høstet fra gruppen ". $groupName . ", og gemt i filen '" . $outputFileName."'";
 	// TODO: Kald insert_into_mysql_database()
 
 	$arrSize = sizeof( $dataArr, 1);
@@ -42,7 +67,7 @@ FacebookSession::setDefaultApplication('294028730792515', 'ea0876ce78b6f88d33d3c
 			// Dette kan vi bruge til at hente flere kommentarer
 			if($next){
 				
-				$morecomments = harvestOneMore( $next );	
+				// $morecomments = harvestOneMore( $next );	
 				// Kald "flere kommentarer service"
 				
 			}
