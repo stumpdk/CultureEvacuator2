a6eeb217c413d765b968251a6494ea6046b6de7e
Jacob Larsen <jac@kb.dk>
2014-10-12 23:53:59 +0200

    added uniqueness to keywords

--- database_structure.sql
+++ database_structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Oct 12, 2014 at 05:43 PM
+-- Generation Time: Oct 12, 2014 at 11:53 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.5.14
 
@@ -28,7 +28,7 @@ CREATE TABLE `ce_posts` (
   `created_time` datetime DEFAULT NULL,
   `updated_time` datetime DEFAULT NULL,
   `message` text
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds posts from Facebook' AUTO_INCREMENT=67 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='#Hack4DK. Holds posts from Facebook' AUTO_INCREMENT=165 ;
 
 --
 -- Indexes for dumped tables
@@ -48,4 +48,10 @@ ALTER TABLE `ce_posts`
 -- AUTO_INCREMENT for table `ce_posts`
 --
 ALTER TABLE `ce_posts`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=67;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=165;
+
+DELETE FROM ce_keywords;
+DELETE FROM ce_keywords_comments;
+DELETE FROM ce_comments;
+DELETE FROM ce_posts
+
